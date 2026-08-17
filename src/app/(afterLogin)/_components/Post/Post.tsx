5c9f64d91e070b447eb59ad259f0276f746e2d13
Conan Kunhwan Ahn <kunhwon@gmail.com>
2026-08-17 17:18:49 +0900

    홈 페이지 - 포스트 컴포넌트 퍼블리싱

---
+++ src/app/(afterLogin)/_components/Post/Post.tsx
@@ -0,0 +1,57 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import "dayjs/locale/ko";
+import Link from "next/link";
+
+import ActionButtons from "../ActionButtons";
+
+import styles from "./Post.module.scss";
+
+dayjs.locale("ko");
+dayjs.extend(relativeTime);
+
+const Post = () => {
+  const target = {
+    user: {
+      id: 'elonmusk',
+      nickname: 'Elon Musk',
+      image: '/yRsRRjGO.jpg',
+    },
+    content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
+    createdAt: new Date(),
+    images: [],
+  }
+
+  return (
+    <article className={styles.post}>
+      <div className={styles.postWrapper}>
+        <div className={styles.postUserSection}>
+          <Link href={`/${target.user.id}`} className={styles.postUserImage}>
+            <img src={target.user.image} alt={target.user.nickname}/>
+            <div className={styles.postShade} />
+          </Link>
+        </div>
+        <div className={styles.postBody}>
+          <div className={styles.postMeta}>
+            <Link href={`/${target.user.id}`}>
+              <span className={styles.postUserName}>{target.user.nickname}</span>
+              &nbsp;
+              <span className={styles.postUserId}>@{target.user.id}</span>
+              &nbsp;
+              ·
+              &nbsp;
+            </Link>
+            <span className={styles.postDate}>{dayjs(target.createdAt).fromNow(true)}</span>
+          </div>
+          <div>{target.content}</div>
+          <div className={styles.postImageSection}>
+
+          </div>
+          <ActionButtons />
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default Post;
